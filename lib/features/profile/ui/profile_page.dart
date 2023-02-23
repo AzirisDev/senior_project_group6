76a72df9e995a5fd48d8753a27e8f9b2bf5af876
azim228228 <azim.s@aloteq.com>
2023-02-23 22:34:25 +0600

    profile page ui

--- lib/features/profile/ui/profile_page.dart
+++ lib/features/profile/ui/profile_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:senior_project_group6/core/utils/appcolors.dart';
+import 'package:senior_project_group6/features/profile/ui/widgets/profile_info_tile.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -10,10 +12,49 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColor.primaryBlue,
+        title: const Text("Profile"),
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.logout_rounded),
+          ),
+        ],
+      ),
       body: SafeArea(
-        child: Center(
-          child: Text("Profile"),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text(
+                "Azim",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w200,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ProfileInfoTile(title: "Role", info: "Student"),
+              SizedBox(
+                height: 12,
+              ),
+              ProfileInfoTile(title: "Email", info: "azim.sattykov@nu.edu.kz"),
+              SizedBox(
+                height: 12,
+              ),
+              ProfileInfoTile(
+                title: "Telephone number",
+                info: "+7 777 686 9559",
+                icon: Icons.phone,
+              ),
+            ],
+          ),
         ),
       ),
     );
