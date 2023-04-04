bef665374bcadbfc6ca2c6239d08b8323f3701fc
azim228228 <azim.s@aloteq.com>
2023-04-05 00:13:43 +0600

    request details

--- lib/core/navigation/router_global.dart
+++ lib/core/navigation/router_global.dart
@@ -4,7 +4,6 @@ import 'package:senior_project_group6/features/auth/sign_up/ui/sign_up_page.dart
 import 'package:senior_project_group6/features/auth/login/ui/login_page.dart';
 
 import '../../features/home/ui/home_page.dart';
-import 'package:senior_project_group6/features/requests/request_overview/ui/request_overview_page.dart';
 
 class RouterGlobal {
   RouterGlobal._();
@@ -17,8 +16,6 @@ class RouterGlobal {
         return MaterialPageRoute(builder: (_) => const HomePage());
       case 'signup':
         return MaterialPageRoute(builder: (_) => const SignUpPage());
-      case 'request_overview':
-        return MaterialPageRoute(builder: (_) => const RequestOverviewPage());
       default:
         return MaterialPageRoute(
             builder: (_) => EmptyPage(message: '${settings.name} not found'));
