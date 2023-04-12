2e986215014b47d2e4a634eeabb8a814f4df2bda
Meiirbek Baimuratov <meirbek.baimuratov@gmail.com>
2023-04-12 23:58:40 +0600

    implement image picker

--- lib/features/create_request/ui/create_request_page.dart
+++ lib/features/create_request/ui/create_request_page.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:multi_image_picker/multi_image_picker.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:senior_project_group6/core/utils/appcolors.dart';
 import 'package:senior_project_group6/core/widgets/generals/custom_text_button.dart';
 import 'package:senior_project_group6/features/create_request/cubit/create_request_cubit.dart';
@@ -22,6 +27,10 @@ class _CreateRequestsPageState extends State<CreateRequestsPage> {
   String? _selectedService;
 
   late CreateRequestCubit _cubit;
+
+  List<File> _selectedImages = [];
+
+  List<Asset> _images = [];
 
   @override
   void initState() {
@@ -65,177 +74,182 @@ class _CreateRequestsPageState extends State<CreateRequestsPage> {
     );
   }
 
-  Column _bodyBuilder(List<Service> services) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 16, right: 16, top: 8),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 10),
-                child: Text(
-                  "Request Type",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w300,
+  Widget _bodyBuilder(List<Service> services) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 16, right: 16, top: 8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    "Request Type",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300,
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
-                decoration: BoxDecoration(color: AppColor.cloudGrey, borderRadius: BorderRadius.circular(10)),
-                child: DropdownButton<String>(
-                  value: _selectedService,
-                  onChanged: (newValue) {
-                    setState(() {
-                      _selectedService = newValue ?? "";
-                    });
-                  },
-                  icon: const Icon(Icons.arrow_drop_down_rounded),
-                  underline: const SizedBox(),
-                  isExpanded: true,
-                  items: services.map((type) {
-                    return DropdownMenuItem(
-                      value: type.departmentType,
-                      child: Text(type.departmentType ?? ''),
-                    );
-                  }).toList(),
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 10),
-                child: Text(
-                  "Title",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w300,
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+                  decoration: BoxDecoration(color: AppColor.cloudGrey, borderRadius: BorderRadius.circular(10)),
+                  child: DropdownButton<String>(
+                    value: _selectedService,
+                    onChanged: (newValue) {
+                      setState(() {
+                        _selectedService = newValue ?? "";
+                      });
+                    },
+                    icon: const Icon(Icons.arrow_drop_down_rounded),
+                    underline: const SizedBox(),
+                    isExpanded: true,
+                    items: services.map((type) {
+                      return DropdownMenuItem(
+                        value: type.departmentType,
+                        child: Text(type.departmentType ?? ''),
+                      );
+                    }).toList(),
                   ),
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
-                decoration: BoxDecoration(color: AppColor.cloudGrey, borderRadius: BorderRadius.circular(10)),
-                child: TextField(
-                  controller: titleController,
-                  decoration: const InputDecoration(
-                    fillColor: AppColor.cloudGrey,
-                    border: InputBorder.none,
-                    hintText: "Give short title",
-                    hintStyle: TextStyle(fontWeight: FontWeight.w300),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    "Title",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300,
+                    ),
                   ),
                 ),
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 10),
-                child: Text(
-                  "Description",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w300,
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+                  decoration: BoxDecoration(color: AppColor.cloudGrey, borderRadius: BorderRadius.circular(10)),
+                  child: TextField(
+                    controller: titleController,
+                    decoration: const InputDecoration(
+                      fillColor: AppColor.cloudGrey,
+                      border: InputBorder.none,
+                      hintText: "Give short title",
+                      hintStyle: TextStyle(fontWeight: FontWeight.w300),
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
-                decoration: BoxDecoration(color: AppColor.cloudGrey, borderRadius: BorderRadius.circular(10)),
-                child: TextField(
-                  controller: descriptionController,
-                  maxLines: 6,
-                  minLines: 3,
-                  decoration: const InputDecoration(
-                    fillColor: AppColor.cloudGrey,
-                    alignLabelWithHint: true,
-                    border: InputBorder.none,
-                    hintText: "Write about the problem",
-                    hintStyle: TextStyle(fontWeight: FontWeight.w300),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    "Description",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300,
+                    ),
                   ),
                 ),
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 10),
-                child: Text(
-                  "Location",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w300,
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+                  decoration: BoxDecoration(color: AppColor.cloudGrey, borderRadius: BorderRadius.circular(10)),
+                  child: TextField(
+                    controller: descriptionController,
+                    maxLines: 6,
+                    minLines: 3,
+                    decoration: const InputDecoration(
+                      fillColor: AppColor.cloudGrey,
+                      alignLabelWithHint: true,
+                      border: InputBorder.none,
+                      hintText: "Write about the problem",
+                      hintStyle: TextStyle(fontWeight: FontWeight.w300),
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
-                decoration: BoxDecoration(color: AppColor.cloudGrey, borderRadius: BorderRadius.circular(10)),
-                child: TextField(
-                  controller: locationController,
-                  decoration: const InputDecoration(
-                    fillColor: AppColor.cloudGrey,
-                    border: InputBorder.none,
-                    hintText: "Enter location",
-                    hintStyle: TextStyle(fontWeight: FontWeight.w300),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    "Location",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300,
+                    ),
                   ),
                 ),
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 10),
-                child: Text(
-                  "Photos",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w300,
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+                  decoration: BoxDecoration(color: AppColor.cloudGrey, borderRadius: BorderRadius.circular(10)),
+                  child: TextField(
+                    controller: locationController,
+                    decoration: const InputDecoration(
+                      fillColor: AppColor.cloudGrey,
+                      border: InputBorder.none,
+                      hintText: "Enter location",
+                      hintStyle: TextStyle(fontWeight: FontWeight.w300),
+                    ),
                   ),
                 ),
-              ),
-            ],
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    "Photos",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-        SizedBox(
-          height: 100,
-          child: Row(
-            children: [
-              Flexible(
-                child: ListView.builder(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  itemCount: 5,
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      margin: const EdgeInsets.only(right: 10),
-                      height: 100,
-                      width: 100,
-                      decoration: BoxDecoration(color: AppColor.cloudGrey, borderRadius: BorderRadius.circular(10)),
-                      child: index == 0
-                          ? const Icon(
-                              Icons.add_a_photo_rounded,
-                              size: 40,
-                              color: AppColor.primaryBlue,
-                            )
-                          : null,
-                    );
-                  },
-                ),
+          Padding(
+            padding: const EdgeInsets.only(left: 16, right: 16),
+            child: CustomTextButton(
+              buttonText: "Choose photos",
+              onPressed: () async {
+                final List<XFile> images = await ImagePicker().pickMultiImage(imageQuality: 80);
+
+                setState(() {
+                  _selectedImages = images.map((image) => File(image.path)).toList();
+                });
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+            child: SizedBox(
+              height: 100,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: _selectedImages.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          margin: const EdgeInsets.only(right: 8.0),
+                          child: Image.file(_selectedImages[index]),
+                        );
+                      },
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
-        const Spacer(),
-        Padding(
-          padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
-          child: CustomTextButton(
-            buttonText: "Create Request",
-            onPressed: () {
-              _cubit.createRequest(
-                description: descriptionController.text,
-                location: locationController.text,
-                requestType: _selectedService ?? '',
-                title: titleController.text,
-              );
-            },
+          Padding(
+            padding: const EdgeInsets.only(left: 16, right: 16, bottom: 0),
+            child: CustomTextButton(
+              buttonText: "Create Request",
+              onPressed: () {
+                _cubit.createRequest(
+                  description: descriptionController.text,
+                  location: locationController.text,
+                  requestType: _selectedService ?? '',
+                  title: titleController.text,
+                );
+              },
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
